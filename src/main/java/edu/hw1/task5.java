c80593288ea690e54a659fcb44db7c4bac21d06b
dolmitya <dolzhenkov_d_a@mail.ru>
2023-10-08 20:37:15 +0300

    да

---
+++ src/main/java/edu/hw1/task5.java
@@ -0,0 +1,44 @@
+package edu.hw1;
+import java.util.Scanner;
+import org.apache.logging.log4j.LogManager;
+public class task5 {
+    private final static org.apache.logging.log4j.Logger LOGGER = LogManager.getLogger();
+    public static int izm(int x) {
+        int result = 0;
+        int mn = 1;
+        while (x > 0) {
+            result += (x % 100 / 10 + x %10) * mn;
+            x = x / 100;
+            mn*=10;
+        }
+        return result;
+    }
+    public static boolean isPalindrome(int integer) {
+        int palindrome = integer;
+        int reverse = 0;
+        while (palindrome != 0) {
+            int remainder = palindrome % 10;
+            reverse = reverse * 10 + remainder;
+            palindrome = palindrome / 10;
+        }
+        return integer == reverse;
+    }
+    public static boolean isPalindromeDescendant(int x) {
+        boolean result=false;
+        while (!result && x>9)
+        {
+            if (isPalindrome(x))
+                result=true;
+            else
+                x=izm(x);
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+        LOGGER.info("Input a digit: ");
+        int x= cin.nextInt();
+        LOGGER.info(isPalindromeDescendant(x));
+        cin.close();
+    }
+}
---
+++ src/test/java/edu/hw1/task5Test.java
@@ -0,0 +1,24 @@
+package edu.hw1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class task5Test {
+
+    @Test
+    void isPalindromeDescendant() {
+        boolean actual=task5.isPalindromeDescendant(11211230);
+        boolean expected = true;
+        assertEquals(expected,actual);
+        actual=task5.isPalindromeDescendant(11211);
+        expected = true;
+        assertEquals(expected,actual);
+        actual=task5.isPalindromeDescendant(211);
+        expected = true;
+        assertEquals(expected,actual);
+        actual=task5.isPalindromeDescendant(21);
+        expected = false;
+        assertEquals(expected,actual);
+    }
+}
