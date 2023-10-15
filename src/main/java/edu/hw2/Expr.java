3cd810063f8dfceec2b606c498e567c890db24fc
dolmitya <dolzhenkov_d_a@mail.ru>
2023-10-15 16:06:35 +0300

    09/10/23 19:40

---
+++ src/main/java/edu/hw2/Expr.java
@@ -0,0 +1,36 @@
+package edu.hw2;
+
+public sealed interface Expr permits Expr.Addition, Expr.Constant, Expr.Negate, Expr.Multiplication, Expr.Exponent {
+    double evaluate();
+
+    public record Constant(double value) implements Expr {
+        public double evaluate() {
+            return value;
+        }
+    }
+
+    public record Addition(Expr first, Expr second) implements Expr {
+        public double evaluate() {
+            return first.evaluate() + second.evaluate();
+        }
+    }
+
+    public record Negate(Expr value) implements Expr {
+        public double evaluate() {
+            return -value.evaluate();
+        }
+    }
+
+    public record Multiplication(Expr first, Expr second) implements Expr {
+        public double evaluate() {
+            return first.evaluate() * second.evaluate();
+        }
+    }
+
+    public record Exponent(Expr val, int n) implements Expr {
+        public double evaluate() {
+            return Math.pow(val.evaluate(), n);
+        }
+    }
+}
+
