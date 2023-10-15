c1c0ee274676b79d0be64414aa12ec8e024fc122
dolmitya <dolzhenkov_d_a@mail.ru>
2023-10-15 23:03:04 +0300

    09/10/23 19:40

---
+++ src/main/java/edu/hw2/Task4.java
@@ -0,0 +1,33 @@
+package edu.hw2;
+
+import java.util.Scanner;
+import org.apache.logging.log4j.LogManager;
+
+@SuppressWarnings("uncommentedmain")
+public class Task4 {
+    private Task4() {
+    }
+
+    private final static org.apache.logging.log4j.Logger LOGGER = LogManager.getLogger();
+
+    public record CallingInfo(String className, String methodName) {
+    }
+
+    public static CallingInfo callingInfo() {
+        try {
+            throw new Exception();
+        } catch (Throwable e) {
+            StackTraceElement[] stktrace = e.getStackTrace();
+            String str1 = stktrace[1].toString();
+            String str = str1.substring(0, str1.indexOf('('));
+            return new CallingInfo(str.substring(0, str.lastIndexOf('.')), str.substring(str.lastIndexOf('.') + 1));
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Scanner cin = new Scanner(System.in);
+        CallingInfo elem = callingInfo();
+        LOGGER.info(elem);
+        cin.close();
+    }
+}
