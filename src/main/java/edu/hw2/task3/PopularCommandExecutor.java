34ce7048cc4e7cedfb83430504cfe92019f4ad29
dolmitya <dolzhenkov_d_a@mail.ru>
2023-10-18 15:03:24 +0300

    18/10/23 14:32

---
+++ src/main/java/edu/hw2/task3/PopularCommandExecutor.java
@@ -0,0 +1,46 @@
+package edu.hw2.task3;
+
+import java.util.Random;
+
+public class PopularCommandExecutor {
+
+    private PopularCommandExecutor() {
+
+    }
+
+    final static String ERROR = "Error";
+    private ConnectManage.ConnectionManager manager;
+    private int maxAttempts;
+
+    PopularCommandExecutor(int maxAttempts) {
+        final int intBOUND = 2;
+        Random random = new Random();
+        manager = (random.nextInt(intBOUND) == 1) ? new ConnectManage.DefaultConnectionManager() :
+            new ConnectManage.FaultyConnectionManager();
+        this.maxAttempts = maxAttempts;
+    }
+
+    public void updatePackages() throws Exception {
+        tryExecute("apt update && apt upgrade -y");
+    }
+
+    void tryExecute(String command) throws Exception {
+        final Connect.Connection connection = manager.getConnection();
+        boolean fl = true;
+        if (maxAttempts == 0) {
+            throw new Task3.ConnectionException(ERROR, new RuntimeException());
+        }
+        for (int cnt = 1; cnt <= maxAttempts && fl; cnt++) {
+            try {
+                connection.execute(command);
+                fl = false;
+            } catch (Task3.ConnectionException e) {
+                fl = true;
+                if (cnt == maxAttempts) {
+                    throw new Task3.ConnectionException(ERROR, new RuntimeException());
+                }
+            }
+        }
+        connection.close();
+    }
+}
