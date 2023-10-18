602d3a469f72a0f8afedaba0267585ab5e687e20
dolmitya <dolzhenkov_d_a@mail.ru>
2023-10-18 15:03:24 +0300

    18/10/23 14:32

---
+++ src/main/java/edu/hw2/task3/ConnectManage.java
@@ -0,0 +1,32 @@
+package edu.hw2.task3;
+
+import java.util.Random;
+
+public class ConnectManage {
+
+    private ConnectManage() {
+    }
+
+    public interface ConnectionManager {
+        Connect.Connection getConnection();
+    }
+
+    public static class DefaultConnectionManager implements ConnectionManager {
+        public final int intBOUND = 3;
+
+        public Connect.Connection getConnection() {
+            Random random = new Random();
+            if (random.nextInt(intBOUND) == 0) {
+                return new Connect.FaultyConnection();
+            } else {
+                return new Connect.StableConnection();
+            }
+        }
+    }
+
+    public static class FaultyConnectionManager implements ConnectionManager {
+        public Connect.Connection getConnection() {
+            return new Connect.FaultyConnection();
+        }
+    }
+}
