da20410ad23b50bdc6a2776b33fa68aad32532b1
dolmitya <dolzhenkov_d_a@mail.ru>
2023-10-29 23:16:52 +0300

    task8ready

--- src/main/java/edu/hw4/Task.java
+++ src/main/java/edu/hw4/Task.java
@@ -1,12 +1,22 @@
 package edu.hw4;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Task {
-    private Task(){
+    private Task() {
     }
-    public List<Animal> task1(List<Animal> animals)
-    {
 
+    public List<Animal> task1(List<Animal> animals) {
+        List<Animal> result = animals.sort((o1, o2) -> {
+            if (o1.height() > o2.height()) {
+                return true;
+            } else if (o1.height() < o2.height()) {
+                return -1;
+            } else {
+                return 0;
+            }
+        });
+        return result;
     }
 }
