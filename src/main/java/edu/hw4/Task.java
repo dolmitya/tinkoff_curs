05789494ec58dbe59506f988d6ba043ce992686c
dolmitya <dolzhenkov_d_a@mail.ru>
2023-10-30 00:00:19 +0300

    task8ready

--- src/main/java/edu/hw4/Task.java
+++ src/main/java/edu/hw4/Task.java
@@ -1,11 +1,17 @@
 package edu.hw4;
 
-import java.util.Comparator;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Task {
-    private Task() {
+    public Task() {
     }
 
     public List<Animal> task1(List<Animal> animals) {
@@ -18,6 +24,26 @@ public class Task {
                 }
                 return 0;
             }).collect(Collectors.toList());
+        return result;
+    }
+
+    public List<Animal> task2(List<Animal> animals) {
+        List<Animal> result = animals.stream()
+            .sorted((o1, o2) -> {
+                if (o1.weight() < o2.weight()) {
+                    return 1;
+                } else if (o1.weight() > o2.weight()) {
+                    return -1;
+                }
+                return 0;
+            }).collect(Collectors.toList());
+        return result;
+    }
+
+    public Map<Animal.Type, Integer> task3(List<Animal> animals) {
+        Map<Animal.Type, Integer> result = new HashMap<Animal.Type, Integer>();
+        Stream stream = animals.stream();
+        stream.filter(x-> x.toString().length() == 3).forEach(System.out::println);
         return result;
     }
 }
--- src/test/java/edu/hw4/TaskTest.java
+++ src/test/java/edu/hw4/TaskTest.java
@@ -3,6 +3,7 @@ package edu.hw4;
 import edu.hw1.Task2;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import java.util.Arrays;
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,8 +11,26 @@ class TaskTest {
     @Test
     @DisplayName("1 задание")
     void task1() {
-        List<Animal> actual = ;
-        int expected = 4;
+        Task task = new Task();
+        Animal anim1=new Animal("1", Animal.Type.CAT,  Animal.Sex.M, 10, 35, 40, true);
+        Animal anim2=new Animal("2", Animal.Type.CAT,  Animal.Sex.M, 10, 30, 40, true);
+        Animal anim3=new Animal("3", Animal.Type.CAT,  Animal.Sex.M, 10, 40, 40, true);
+        List<Animal> list = Arrays.asList(anim1, anim2, anim3);
+        String actual = task.task1(list).toString();
+        String expected = "[Animal[name=2, type=CAT, sex=M, age=10, height=30, weight=40, bites=true], Animal[name=1, type=CAT, sex=M, age=10, height=35, weight=40, bites=true], Animal[name=3, type=CAT, sex=M, age=10, height=40, weight=40, bites=true]]";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("2 задание")
+    void task2() {
+        Task task = new Task();
+        Animal anim1=new Animal("1", Animal.Type.CAT,  Animal.Sex.M, 10, 35, 40, true);
+        Animal anim2=new Animal("2", Animal.Type.CAT,  Animal.Sex.M, 10, 30, 45, true);
+        Animal anim3=new Animal("3", Animal.Type.CAT,  Animal.Sex.M, 10, 40, 50, true);
+        List<Animal> list = Arrays.asList(anim1, anim2, anim3);
+        String actual = task.task1(list).toString();
+        String expected = "[Animal[name=2, type=CAT, sex=M, age=10, height=30, weight=45, bites=true], Animal[name=1, type=CAT, sex=M, age=10, height=35, weight=40, bites=true], Animal[name=3, type=CAT, sex=M, age=10, height=40, weight=50, bites=true]";
         assertEquals(expected, actual);
     }
 }
