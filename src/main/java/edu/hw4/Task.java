96f7f354b4f96dde086142b2a4efb550371b6999
dolmitya <dolzhenkov_d_a@mail.ru>
2023-10-31 03:35:39 +0300

    task8ready

--- src/main/java/edu/hw4/Task.java
+++ src/main/java/edu/hw4/Task.java
@@ -1,19 +1,17 @@
 package edu.hw4;
 
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import java.util.Collection;
-import java.util.HashMap;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
+import java.util.Optional;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Task {
     public Task() {
     }
 
+    //Отсортировать животных по росту от самого маленького к
+    //самому большому -> List<Animal>
     public List<Animal> task1(List<Animal> animals) {
         List<Animal> result = animals.stream()
             .sorted((o1, o2) -> {
@@ -27,6 +25,8 @@ public class Task {
         return result;
     }
 
+    //Отсортировать животных по весу от самого тяжелого к самому
+    //легкому, выбрать k первых -> List<Animal>
     public List<Animal> task2(List<Animal> animals) {
         List<Animal> result = animals.stream()
             .sorted((o1, o2) -> {
@@ -40,10 +40,153 @@ public class Task {
         return result;
     }
 
-    public Map<Animal.Type, Integer> task3(List<Animal> animals) {
-        Map<Animal.Type, Integer> result = new HashMap<Animal.Type, Integer>();
-        Stream stream = animals.stream();
-        stream.filter(x-> x.toString().length() == 3).forEach(System.out::println);
+    //Сколько животных каждого вида -> Map<Animal.Type, Integer>
+    public Map<Animal.Type, Long> task3(List<Animal> animals) {
+        Map<Animal.Type, Long> result = animals.stream().collect(
+            Collectors.groupingBy(Animal::type, Collectors.counting()));
         return result;
+    }
+
+    //У какого животного самое длинное имя -> Animal
+    public Animal task4(List<Animal> animals) {
+        Animal result = animals.stream()
+            .max(Comparator.comparing(animal -> animal.name().length()))
+            .orElse(null);
+        return result;
+    }
+
+    //Каких животных больше: самцов или самок -> Sex
+    public Animal.Sex task5(List<Animal> animals) {
+        Map<Animal.Sex, Long> pol = animals.stream()
+            .collect(Collectors.groupingBy(
+                Animal::sex,
+                Collectors.counting()
+            ));
+        return pol.get(Animal.Sex.M) > pol.get(Animal.Sex.F) ? Animal.Sex.M
+            : pol.get(Animal.Sex.M) < pol.get(Animal.Sex.F) ? Animal.Sex.F : null;
+    }
+
+    //Самое тяжелое животное каждого вида -> Map<Animal.Type, Animal>
+    public Map<Animal.Type, Optional<Animal>> task6(List<Animal> animals) {
+        return animals.stream().collect(
+            Collectors.groupingBy(
+                Animal::type,
+                Collectors.maxBy(Comparator.comparing(Animal::weight))
+            ));
+    }
+
+    //K-е самое старое животное -> Animal
+    public Animal task7(List<Animal> animals, int k) {
+        List<Animal> list = animals.stream()
+            .sorted((o1, o2) -> {
+                if (o1.age() < o2.age()) {
+                    return 1;
+                } else if (o1.age() > o2.age()) {
+                    return -1;
+                }
+                return 0;
+            }).collect(Collectors.toList());
+        return (k > 0 && k < list.size()) ? list.get(k) : null;
+    }
+
+    //Самое тяжелое животное среди животных ниже k см ->
+    //Optional<Animal>
+    public Animal task8(List<Animal> animals, int k) {
+        return animals.stream().filter(animal -> animal.height() < k)
+            .max(Comparator.comparing(Animal::weight)).get();
+    }
+
+    //Сколько в сумме лап у животных в списке -> Integer
+    public Integer task9(List<Animal> animals) {
+        return animals.stream().collect(Collectors.summingInt(Animal::paws));
+    }
+
+    //Список животных, возраст у которых не совпадает с количеством
+    //лап -> List<Animal>
+    public List<Animal> task10(List<Animal> animals) {
+        return animals.stream().filter(animal -> animal.age() != animal.paws()).toList();
+    }
+
+    //Список животных, которые могут укусить (bites == null или true) и
+    //рост которых превышает 100 см -> List<Animal>
+    public List<Animal> task11(List<Animal> animals) {
+        return animals.stream().filter(animal -> animal.bites() && animal.height() > 100).toList();
+    }
+
+    //Сколько в списке животных, вес которых превышает рост ->
+    //Integer
+    public Integer task12(List<Animal> animals) {
+        return animals.stream().filter(animal -> animal.weight() > animal.height()).toList().size();
+    }
+
+    //Список животных, имена которых состоят из более чем двух слов
+    //-> List<Animal>
+    public List<Animal> task13(List<Animal> animals) {
+        return animals.stream().filter(animal -> animal.name().indexOf(' ') > -1).toList();
+    }
+
+    //Есть ли в списке собака ростом более k см -> Boolean
+    public boolean task14(List<Animal> animals, int k) {
+        return !animals.stream().filter(animal -> animal.type() == Animal.Type.DOG
+            && animal.height() > k).toList().isEmpty();
+    }
+
+    //Найти суммарный вес животных каждого вида, которым от k до l
+    //лет -> Integer
+    public Integer task15(List<Animal> animals, int k, int l) {
+        return animals.stream().filter(animal -> animal.age() > k && animal.age() < l)
+            .collect(Collectors.summingInt(Animal::weight));
+    }
+
+    //Список животных, отсортированный по виду,
+    //затем по полу, затем по имени -> List<Animal>
+    public List<Animal> task16(List<Animal> animals) {
+        return animals.stream()
+            .sorted(Comparator.comparing(Animal::type))
+            .sorted(Comparator.comparing(Animal::sex))
+            .sorted(Comparator.comparing(Animal::name))
+            .collect(Collectors.toList());
+    }
+
+    //Правда ли, что пауки кусаются чаще, чем собаки -> Boolean (если
+    // данных для ответа недостаточно, вернуть false)
+    public boolean task17(List<Animal> animals) {
+        int Sbites = animals.stream()
+            .filter(animal -> animal.type() == Animal.Type.SPIDER && animal.bites())
+            .toList().size();
+        int Dbites = animals.stream()
+            .filter(animal -> animal.type() == Animal.Type.DOG && animal.bites())
+            .toList().size();
+        if (Sbites == 0 || Dbites == 0) {
+            return false;
+        }
+        return Sbites > Dbites;
+    }
+
+    //Найти самую тяжелую рыбку в 2-х или более списках -> Animal
+    public Animal task18(List<List<Animal>> animals) {
+        if (animals.isEmpty())
+            return null;
+        Animal maxFish = null, maxFish0;
+        maxFish0 = animals.get(0).stream()
+            .filter(animal -> animal.type() == Animal.Type.FISH)
+            .max(Comparator.comparing(Animal::weight)).get();
+        for (int i=1; i< animals.size(); i++)
+        {
+            if (maxFish0 == animals.get(i).stream()
+                .filter(animal -> animal.type() == Animal.Type.FISH)
+                .max(Comparator.comparing(Animal::weight)).get()){
+                maxFish=maxFish0;
+            }
+
+            if (maxFish0.weight() < animals.get(i).stream()
+                .filter(animal -> animal.type() == Animal.Type.FISH)
+                .max(Comparator.comparing(Animal::weight)).get().weight()){
+                maxFish0 = animals.get(i).stream()
+                    .filter(animal -> animal.type() == Animal.Type.FISH)
+                    .max(Comparator.comparing(Animal::weight)).get();
+            }
+        }
+        return maxFish;
     }
 }
--- src/test/java/edu/hw4/TaskTest.java
+++ src/test/java/edu/hw4/TaskTest.java
@@ -1,6 +1,5 @@
 package edu.hw4;
 
-import edu.hw1.Task2;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import java.util.Arrays;
@@ -12,12 +11,14 @@ class TaskTest {
     @DisplayName("1 задание")
     void task1() {
         Task task = new Task();
-        Animal anim1=new Animal("1", Animal.Type.CAT,  Animal.Sex.M, 10, 35, 40, true);
-        Animal anim2=new Animal("2", Animal.Type.CAT,  Animal.Sex.M, 10, 30, 40, true);
-        Animal anim3=new Animal("3", Animal.Type.CAT,  Animal.Sex.M, 10, 40, 40, true);
+        Animal anim1 = new Animal("1", Animal.Type.CAT, Animal.Sex.M, 10, 35, 40, true);
+        Animal anim2 = new Animal("2", Animal.Type.CAT, Animal.Sex.M, 10, 30, 40, true);
+        Animal anim3 = new Animal("3", Animal.Type.CAT, Animal.Sex.M, 10, 40, 40, true);
         List<Animal> list = Arrays.asList(anim1, anim2, anim3);
         String actual = task.task1(list).toString();
-        String expected = "[Animal[name=2, type=CAT, sex=M, age=10, height=30, weight=40, bites=true], Animal[name=1, type=CAT, sex=M, age=10, height=35, weight=40, bites=true], Animal[name=3, type=CAT, sex=M, age=10, height=40, weight=40, bites=true]]";
+        String expected = "[Animal[name=2, type=CAT, sex=M, age=10, height=30, weight=40, bites=true], " +
+            "Animal[name=1, type=CAT, sex=M, age=10, height=35, weight=40, bites=true], " +
+            "Animal[name=3, type=CAT, sex=M, age=10, height=40, weight=40, bites=true]]";
         assertEquals(expected, actual);
     }
 
@@ -25,12 +26,250 @@ class TaskTest {
     @DisplayName("2 задание")
     void task2() {
         Task task = new Task();
-        Animal anim1=new Animal("1", Animal.Type.CAT,  Animal.Sex.M, 10, 35, 40, true);
-        Animal anim2=new Animal("2", Animal.Type.CAT,  Animal.Sex.M, 10, 30, 45, true);
-        Animal anim3=new Animal("3", Animal.Type.CAT,  Animal.Sex.M, 10, 40, 50, true);
+        Animal anim1 = new Animal("1", Animal.Type.CAT, Animal.Sex.M, 10, 35, 40, true);
+        Animal anim2 = new Animal("2", Animal.Type.CAT, Animal.Sex.M, 10, 30, 45, true);
+        Animal anim3 = new Animal("3", Animal.Type.CAT, Animal.Sex.M, 10, 40, 50, true);
         List<Animal> list = Arrays.asList(anim1, anim2, anim3);
-        String actual = task.task1(list).toString();
-        String expected = "[Animal[name=2, type=CAT, sex=M, age=10, height=30, weight=45, bites=true], Animal[name=1, type=CAT, sex=M, age=10, height=35, weight=40, bites=true], Animal[name=3, type=CAT, sex=M, age=10, height=40, weight=50, bites=true]";
+        String actual = task.task2(list).toString();
+        String expected = "[Animal[name=3, type=CAT, sex=M, age=10, height=40, weight=50, bites=true], " +
+            "Animal[name=2, type=CAT, sex=M, age=10, height=30, weight=45, bites=true], " +
+            "Animal[name=1, type=CAT, sex=M, age=10, height=35, weight=40, bites=true]]";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("3 задание")
+    void task3() {
+        Task task = new Task();
+        Animal anim1 = new Animal("1", Animal.Type.CAT, Animal.Sex.M, 10, 35, 40, true);
+        Animal anim2 = new Animal("2", Animal.Type.CAT, Animal.Sex.M, 10, 30, 45, true);
+        Animal anim3 = new Animal("3", Animal.Type.CAT, Animal.Sex.M, 10, 40, 50, true);
+        List<Animal> list = Arrays.asList(anim1, anim2, anim3);
+        String actual = task.task3(list).toString();
+        String expected = "{CAT=3}";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("4 задание")
+    void task4() {
+        Task task = new Task();
+        Animal anim1 = new Animal("Barsik", Animal.Type.CAT, Animal.Sex.M, 10, 35, 40, true);
+        Animal anim2 = new Animal("Sasha", Animal.Type.CAT, Animal.Sex.M, 10, 30, 45, true);
+        Animal anim3 = new Animal("Pushoki", Animal.Type.CAT, Animal.Sex.M, 10, 40, 50, true);
+        List<Animal> list = Arrays.asList(anim1, anim2, anim3);
+        String actual = task.task4(list).toString();
+        String expected = "Animal[name=Pushoki, type=CAT, sex=M, age=10, height=40, weight=50, bites=true]";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("5 задание")
+    void task5() {
+        Task task = new Task();
+        Animal anim1 = new Animal("Barsik", Animal.Type.CAT, Animal.Sex.M, 10, 35, 40, true);
+        Animal anim2 = new Animal("Sasha", Animal.Type.CAT, Animal.Sex.F, 10, 30, 45, true);
+        Animal anim3 = new Animal("Pushoki", Animal.Type.CAT, Animal.Sex.M, 10, 40, 50, true);
+        Animal anim4 = new Animal("Pushoki", Animal.Type.CAT, Animal.Sex.F, 10, 40, 50, true);
+        List<Animal> list = Arrays.asList(anim1, anim2, anim3, anim4);
+        Animal.Sex actual = task.task5(list);
+        Animal.Sex expected = null;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("6 задание")
+    void task6() {
+        Task task = new Task();
+        Animal anim1 = new Animal("Barsik", Animal.Type.CAT, Animal.Sex.M, 10, 35, 50, true);
+        Animal anim2 = new Animal("Sasha", Animal.Type.CAT, Animal.Sex.F, 10, 30, 45, true);
+        Animal anim3 = new Animal("Pushoki", Animal.Type.DOG, Animal.Sex.M, 10, 40, 50, true);
+        Animal anim4 = new Animal("Pushoki", Animal.Type.FISH, Animal.Sex.F, 10, 40, 50, true);
+        List<Animal> list = Arrays.asList(anim1, anim2, anim3, anim4);
+        String actual = task.task6(list).toString();
+        String expected =
+            "{CAT=Optional[Animal[name=Barsik, type=CAT, sex=M, age=10, height=35, weight=50, bites=true]], " +
+                "DOG=Optional[Animal[name=Pushoki, type=DOG, sex=M, age=10, height=40, weight=50, bites=true]], " +
+                "FISH=Optional[Animal[name=Pushoki, type=FISH, sex=F, age=10, height=40, weight=50, bites=true]]}";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("7 задание")
+    void task7() {
+        Task task = new Task();
+        Animal anim1 = new Animal("Barsik", Animal.Type.CAT, Animal.Sex.M, 12, 35, 50, true);
+        Animal anim2 = new Animal("Sasha", Animal.Type.CAT, Animal.Sex.F, 10, 30, 45, true);
+        Animal anim3 = new Animal("Pushoki", Animal.Type.DOG, Animal.Sex.M, 11, 40, 50, true);
+        Animal anim4 = new Animal("Pushoki", Animal.Type.FISH, Animal.Sex.F, 15, 40, 50, true);
+        List<Animal> list = Arrays.asList(anim1, anim2, anim3, anim4);
+        String actual = task.task7(list, 2).toString();
+        String expected = "Animal[name=Pushoki, type=DOG, sex=M, age=11, height=40, weight=50, bites=true]";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("8 задание")
+    void task8() {
+        Task task = new Task();
+        Animal anim1 = new Animal("Barsik", Animal.Type.CAT, Animal.Sex.M, 12, 35, 50, true);
+        Animal anim2 = new Animal("Sasha", Animal.Type.CAT, Animal.Sex.F, 10, 30, 55, true);
+        Animal anim3 = new Animal("Pushoki", Animal.Type.DOG, Animal.Sex.M, 11, 39, 50, true);
+        Animal anim4 = new Animal("Pushoki", Animal.Type.FISH, Animal.Sex.F, 15, 40, 50, true);
+        List<Animal> list = Arrays.asList(anim1, anim2, anim3, anim4);
+        String actual = task.task8(list, 40).toString();
+        String expected = "Animal[name=Sasha, type=CAT, sex=F, age=10, height=30, weight=55, bites=true]";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("9 задание")
+    void task9() {
+        Task task = new Task();
+        Animal anim1 = new Animal("Barsik", Animal.Type.CAT, Animal.Sex.M, 12, 35, 50, true);
+        Animal anim2 = new Animal("Sasha", Animal.Type.CAT, Animal.Sex.F, 10, 30, 55, true);
+        Animal anim3 = new Animal("Pushoki", Animal.Type.DOG, Animal.Sex.M, 11, 39, 50, true);
+        Animal anim4 = new Animal("Pushoki", Animal.Type.FISH, Animal.Sex.F, 15, 40, 50, true);
+        List<Animal> list = Arrays.asList(anim1, anim2, anim3, anim4);
+        Integer actual = task.task9(list);
+        Integer expected = 12;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("10 задание")
+    void task10() {
+        Task task = new Task();
+        Animal anim1 = new Animal("Barsik", Animal.Type.CAT, Animal.Sex.M, 4, 35, 50, true);
+        Animal anim2 = new Animal("Sasha", Animal.Type.CAT, Animal.Sex.F, 10, 30, 55, true);
+        Animal anim3 = new Animal("Pushoki", Animal.Type.DOG, Animal.Sex.M, 4, 39, 50, true);
+        Animal anim4 = new Animal("Pushoki", Animal.Type.FISH, Animal.Sex.F, 15, 40, 50, true);
+        List<Animal> list = Arrays.asList(anim1, anim2, anim3, anim4);
+        String actual = task.task10(list).toString();
+        String expected = "[Animal[name=Sasha, type=CAT, sex=F, age=10, height=30, weight=55, bites=true], " +
+            "Animal[name=Pushoki, type=FISH, sex=F, age=15, height=40, weight=50, bites=true]]";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("11 задание")
+    void task11() {
+        Task task = new Task();
+        Animal anim1 = new Animal("Barsik", Animal.Type.CAT, Animal.Sex.M, 4, 35, 50, true);
+        Animal anim2 = new Animal("Sasha", Animal.Type.CAT, Animal.Sex.F, 10, 101, 55, true);
+        Animal anim3 = new Animal("Pushoki", Animal.Type.DOG, Animal.Sex.M, 4, 460, 50, false);
+        Animal anim4 = new Animal("Pushoki", Animal.Type.FISH, Animal.Sex.F, 15, 120, 50, true);
+        List<Animal> list = Arrays.asList(anim1, anim2, anim3, anim4);
+        String actual = task.task11(list).toString();
+        String expected = "[Animal[name=Sasha, type=CAT, sex=F, age=10, height=101, weight=55, bites=true], " +
+            "Animal[name=Pushoki, type=FISH, sex=F, age=15, height=120, weight=50, bites=true]]";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("12 задание")
+    void task12() {
+        Task task = new Task();
+        Animal anim1 = new Animal("Barsik", Animal.Type.CAT, Animal.Sex.M, 4, 35, 50, true);
+        Animal anim2 = new Animal("Sasha", Animal.Type.CAT, Animal.Sex.F, 10, 101, 55, true);
+        Animal anim3 = new Animal("Pushoki", Animal.Type.DOG, Animal.Sex.M, 4, 460, 50, false);
+        Animal anim4 = new Animal("Pushoki", Animal.Type.FISH, Animal.Sex.F, 15, 120, 50, true);
+        List<Animal> list = Arrays.asList(anim1, anim2, anim3, anim4);
+        Integer actual = task.task12(list);
+        Integer expected = 1;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("13 задание")
+    void task13() {
+        Task task = new Task();
+        Animal anim1 = new Animal("Bar sik", Animal.Type.CAT, Animal.Sex.M, 4, 35, 50, true);
+        Animal anim2 = new Animal("Sa sh a", Animal.Type.CAT, Animal.Sex.F, 10, 101, 55, true);
+        Animal anim3 = new Animal("Pushoki", Animal.Type.DOG, Animal.Sex.M, 4, 460, 50, false);
+        Animal anim4 = new Animal("Pushoki", Animal.Type.FISH, Animal.Sex.F, 15, 120, 50, true);
+        List<Animal> list = Arrays.asList(anim1, anim2, anim3, anim4);
+        String actual = task.task13(list).toString();
+        String expected = "[Animal[name=Bar sik, type=CAT, sex=M, age=4, height=35, weight=50, bites=true], " +
+            "Animal[name=Sa sh a, type=CAT, sex=F, age=10, height=101, weight=55, bites=true]]";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("14 задание")
+    void task14() {
+        Task task = new Task();
+        Animal anim1 = new Animal("Barsik", Animal.Type.CAT, Animal.Sex.M, 4, 35, 50, true);
+        Animal anim2 = new Animal("Sasha", Animal.Type.CAT, Animal.Sex.F, 10, 101, 55, true);
+        Animal anim3 = new Animal("Pushok", Animal.Type.DOG, Animal.Sex.M, 4, 460, 50, false);
+        Animal anim4 = new Animal("Sashok", Animal.Type.FISH, Animal.Sex.F, 15, 120, 50, true);
+        List<Animal> list = Arrays.asList(anim1, anim2, anim3, anim4);
+        boolean actual = task.task14(list, 450);
+        boolean expected = true;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("15 задание")
+    void task15() {
+        Task task = new Task();
+        Animal anim1 = new Animal("Barsik", Animal.Type.CAT, Animal.Sex.M, 4, 35, 50, true);
+        Animal anim2 = new Animal("Sasha", Animal.Type.CAT, Animal.Sex.F, 10, 101, 55, true);
+        Animal anim3 = new Animal("Pushok", Animal.Type.DOG, Animal.Sex.M, 4, 460, 50, false);
+        Animal anim4 = new Animal("Sashok", Animal.Type.FISH, Animal.Sex.F, 15, 120, 50, true);
+        List<Animal> list = Arrays.asList(anim1, anim2, anim3, anim4);
+        Integer actual = task.task15(list, 9, 16);
+        Integer expected = 105;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("16 задание")
+    void task16() {
+        Task task = new Task();
+        Animal anim1 = new Animal("Barsik", Animal.Type.CAT, Animal.Sex.M, 4, 35, 50, true);
+        Animal anim2 = new Animal("Sasha", Animal.Type.CAT, Animal.Sex.F, 10, 101, 55, true);
+        Animal anim3 = new Animal("Pushoki", Animal.Type.DOG, Animal.Sex.M, 4, 460, 50, false);
+        Animal anim4 = new Animal("Pushoki", Animal.Type.FISH, Animal.Sex.F, 15, 120, 50, true);
+        List<Animal> list = Arrays.asList(anim1, anim2, anim3, anim4);
+        String actual = task.task16(list).toString();
+        String expected = "[Animal[name=Barsik, type=CAT, sex=M, age=4, height=35, weight=50, bites=true], " +
+            "Animal[name=Pushoki, type=DOG, sex=M, age=4, height=460, weight=50, bites=false], " +
+            "Animal[name=Pushoki, type=FISH, sex=F, age=15, height=120, weight=50, bites=true], " +
+            "Animal[name=Sasha, type=CAT, sex=F, age=10, height=101, weight=55, bites=true]]";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("17 задание")
+    void task17() {
+        Task task = new Task();
+        Animal anim1 = new Animal("Barsik", Animal.Type.SPIDER, Animal.Sex.M, 4, 35, 50, true);
+        Animal anim2 = new Animal("Sasha", Animal.Type.SPIDER, Animal.Sex.F, 10, 101, 55, true);
+        Animal anim3 = new Animal("Pushoki", Animal.Type.DOG, Animal.Sex.M, 4, 460, 50, false);
+        Animal anim4 = new Animal("Pushoki", Animal.Type.DOG, Animal.Sex.F, 15, 120, 50, true);
+        List<Animal> list = Arrays.asList(anim1, anim2, anim3, anim4);
+        boolean actual = task.task17(list);
+        assertTrue(actual);
+    }
+
+    @Test
+    @DisplayName("18 задание")
+    void task18() {
+        Task task = new Task();
+        Animal anim1 = new Animal("Barsik", Animal.Type.FISH, Animal.Sex.M, 4, 35, 50, true);
+        Animal anim2 = new Animal("Sasha", Animal.Type.FISH, Animal.Sex.F, 10, 101, 55, true);
+        Animal anim3 = new Animal("Pushoki", Animal.Type.FISH, Animal.Sex.M, 4, 460, 50, false);
+        Animal anim4 = new Animal("Pushoki", Animal.Type.FISH, Animal.Sex.F, 15, 120, 50, true);
+        Animal anim5 = new Animal("Pushoki", Animal.Type.FISH, Animal.Sex.F, 15, 120, 50, true);
+        Animal anim6 = new Animal("Pushoki", Animal.Type.FISH, Animal.Sex.F, 15, 120, 50, true);
+        Animal anim7 = new Animal("Pushoki", Animal.Type.FISH, Animal.Sex.F, 15, 120, 50, true);
+        List<Animal> list1 = Arrays.asList(anim1, anim2, anim3, anim4);
+        List<Animal> list2 = Arrays.asList(anim2, anim3, anim4, anim5);
+        List<Animal> list3 = Arrays.asList(anim4, anim5, anim6, anim7);
+        List<List<Animal>> list = Arrays.asList(list1, list2, list3);
+        Animal actual = task.task18(list);
+        Animal expected = anim2;
         assertEquals(expected, actual);
     }
 }
